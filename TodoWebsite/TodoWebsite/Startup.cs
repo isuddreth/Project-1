64d1a04392c0b37cb87ff3124162bf6950cb85dd
Ian Suddreth <iansuddreth@gmail.com>
2017-01-15 10:01:11 -0700

    Added ASP.NET Website

---
+++ TodoWebsite/TodoWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TodoWebsite.Startup))]
+namespace TodoWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
